deeba2b4441d1e52c1e24effcc38ae6af1b8f8a0
TechNguyen <ndt13102003@gmail.com>
2023-01-21 12:08:48 +0700

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const items = Array.from(document.querySelectorAll('.number'));
+const body = document.querySelector('body')
 const show = document.querySelector('.caculator-show');
 const funcItem = Array.from(document.querySelectorAll('.func'));
+const acbtn = document.querySelector('.acbtn')
+const delbtn = document.querySelector('.delbtn')
+const ansbtn = document.querySelector('.ansbtn')
 const cacul = document.querySelector('.handle-cacul');
 const active = document.querySelector('.active');
 const result = document.getElementById('text-result');
+const notif = document.querySelector('.notif');
+const close = document.querySelector('.close');
+const progress = document.querySelector('.progress')
+var rect = notif.getBoundingClientRect();
+
 var fnc = ['+', '-', 'x', '/'];
 var fncExp = ['x', '/']
 var stringCurent = '';
@@ -45,8 +54,6 @@ function createPn() {
 active.addEventListener('click', () => {
     const operators = Array.from(document.querySelectorAll('.operator'));
     const fncs = Array.from(document.querySelectorAll('.fnc-opera'))
-    var sum = 0;
-    var sign = 0 ;
     var acc = 0;
     var share = 0;
     var opArr = [];
@@ -60,12 +67,26 @@ active.addEventListener('click', () => {
     });
     resultItem = opArr[0];
     for(let i = 0; i < opArr.length; i++) {
-        for(let j = 0; j < fncArr.length; j++) {
-            fncExp.forEach(item => {
-                if(fncArr.includes(item)) {
-                    console.log(item);
+        do {
+            for(let j = 0; j < fncArr.length; j++) {
+                if(fncArr[j] == 'x') {
+                    acc = opArr[j];
+                    acc *= opArr[j+1];
+                    opArr[j] = acc;
+                    opArr.splice(j+1,1)
+                    fncArr.splice(j, 1) 
                 }
-            })
+                else if(fncArr[j] == '/') {
+                    share = opArr[j];
+                    share /= opArr[j+1];
+                    opArr[j] = share;
+                    opArr.splice(j+1,1)
+                    fncArr.splice(j, 1)
+                }
+                resultItem = opArr[0]
+            }
+        } while(fncArr.includes('x') || fncArr.includes('/'));
+        for(let j = 0; j < fncArr.length; j++) {
             if(fncArr[j] == '+') {
                 resultItem += opArr[i+1];
                 opArr[0] = resultItem;
@@ -88,35 +109,49 @@ active.addEventListener('click', () => {
             }
         }
     }
-    result.innerText = `${resultItem}`
+    if(!isFinite(resultItem)) {
+        notif.style.animation = "shownoti 2s linear forwards";
+        while(cacul.lastChild) {
+            cacul.removeChild(cacul.lastChild)
+        }
+        stringCurent = '';
+        if(notif.style.animationName == "shownoti") {
+            setTimeout(() => {
+                notif.style.animation = "hidennoti 2s linear forwards";
+            }, 5000);
+        }
+        if(notif.style.transform == 0) {
+            progress.style.animation = 'transition 2s linear 3s forwards'
+        }
+    } else {
+        result.innerText = `${resultItem}`
+    }
+})
+
+close.addEventListener('click', function () {
+    notif.style.animation = "hidennoti 2s linear forwards";
+})
+delbtn.addEventListener('click', function () {
+    cacul.lastChild.innerText = `${ cacul.lastChild.innerText.slice(0, cacul.lastChild.innerText.length - 1)}`;
+    stringCurent =  stringCurent.slice(0, stringCurent.length - 1)
+    if(cacul.lastChild.innerText == '') {
+        cacul.removeChild(cacul.lastChild);
+    }
+})
+var child;
+acbtn.addEventListener('click', function() {
+    child = result.innerText;
+    while (cacul.lastChild) {
+        cacul.removeChild(cacul.lastChild);
+      }
+    stringCurent = '';
+    result.innerText = 0;
+})
+var lastchild = document.createElement('p');
+lastchild.className = 'operator'
+ansbtn.addEventListener('click', function () {
+    lastchild.innerText = child;
+    cacul.append(lastchild)
 })
 
 
-// function result(result, item , opArr) {
-//     switch (item) {
-//         case '+':
-//             result += opArr[i+1];
-//             opArr[0] = result;
-//             opArr.splice(i+1, 1);
-//             break;
-//         case '-':
-//             result -= opArr[i+1];
-//             opArr[0] = result;
-//             opArr.splice(i+1, 1);
-//             break;
-//         case '*':
-//             result *= opArr[i+1];
-//             opArr[0] = result;
-//             opArr.splice(i+1, 1); 
-//             break;
-//         case '/':
-//             result /= opArr[i+1];
-//             opArr[0] = result;
-//             opArr.splice(i+1, 1); 
-//             break;
-//         default:
-
-//             break;
-//         }
-
-// }
