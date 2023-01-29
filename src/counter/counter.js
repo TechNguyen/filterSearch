d3ba75be66b61c02f826b0edb6291fcb152c47db
TechNguyen <ndt13102003@gmail.com>
2023-01-29 15:43:48 +0700

    first tiem

--- src/counter/counter.js
+++ src/counter/counter.js
@@ -1,51 +1,32 @@
 import classNames from 'classnames/bind'
 import Style from './counter.module.scss'
 import { useState } from 'react'
+import JSONDATA from './MOCK_DATA.json'
 const cx = classNames.bind(Style)
 function Counter() {
-    const [temperatureValue, setTemperatureValue] = useState(10)
-    const [temperatureColor, setTemperaturColor] = useState([
-        { cold: true },
-        { hot: false },
-    ])
-    const handleIncrease = () => {
-        const newtemperatureValue = temperatureValue + 1
-        if (temperatureValue > 29) return
-        else if (newtemperatureValue >= 15) {
-            setTemperaturColor([{ cold: false }, { hot: true }])
-            setTemperatureValue(newtemperatureValue)
-        } else if (temperatureValue >= 7) {
-            setTemperaturColor([{ cold: true }, { hot: false }])
-            setTemperatureValue(newtemperatureValue)
-        } else return
-    }
-    const handleDecrease = () => {
-        const newtemperatureValue = temperatureValue - 1
-        if (temperatureValue > 30) return
-        else if (newtemperatureValue >= 15) {
-            setTemperaturColor([{ cold: false }, { hot: true }])
-            setTemperatureValue(newtemperatureValue)
-        } else if (temperatureValue > 7) {
-            setTemperaturColor([{ cold: true }, { hot: false }])
-            setTemperatureValue(newtemperatureValue)
-        } else return
-    }
-
+    const [input, setInput] = useState('')
     return (
         <div className={cx('counter-wrapper')}>
-            <div className={cx('counter-content', temperatureColor)}>
-                <p className={cx('temperatureValue')}> {`${temperatureValue}`}</p>
-                <p className={cx('degred')}>
-                    <sup>0</sup>C
-                </p>
-            </div>
-            <div className="handleBtn">
-                <button onClick={handleIncrease} className={cx('Incre-btn')}>
-                    Increase
-                </button>
-                <button onClick={handleDecrease} className={cx('Decre-btn')}>
-                    Decrease
-                </button>
+            <div className={cx('counter-content')}>
+                <input
+                    placeholder="...Search"
+                    onChange={(e) => {
+                        setInput(e.target.value)
+                    }}
+                />
+                <div className={cx('data-list')}>
+                    {JSONDATA.filter((data) => {
+                        if (input == '') {
+                            return
+                        } else if (
+                            data.first_name.toUpperCase().includes(input.toUpperCase())
+                        ) {
+                            return data
+                        }
+                    }).map((data) => (
+                        <p key={data.id}>{data.first_name}</p>
+                    ))}
+                </div>
             </div>
         </div>
     )
